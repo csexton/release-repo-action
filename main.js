57ee5cae0df8ae019404d2cc5045450f8bc28bcc
Christopher Sexton <csexton@users.noreply.github.com>
2019-10-15 15:47:04 -0400

    Convert to a JS action
    Want access to the octokit objects from JS.

---
+++ main.js
@@ -0,0 +1,10 @@
+
+//const core = require('@actions/core')
+//const {GitHub, context} = require('@actions/github')
+
+const token = process.env.GITHUB_TOKEN;
+const repo = "RadiusNetworks/flybuy-ios";
+
+repoURL = `https://${token}@github.com/${repo}.git`;
+const simpleGit = require('simple-git')('.');
+simpleGit.clone(repoURL, "repo", ['--depth=1'])
