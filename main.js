40dcc8728c2f30d0800c0406155aacd3b310273b
Christopher Sexton <csexton@users.noreply.github.com>
2019-10-18 11:39:26 -0400

    WIP

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-
-//const core = require('@actions/core')
+const core = require('@actions/core')
 //const {GitHub, context} = require('@actions/github')
 
-const token = process.env.GITHUB_TOKEN;
-const repo = "RadiusNetworks/flybuy-ios";
+process.on('unhandledRejection', handleError);
+main().catch(handleError);
+function handleError(err) {
+  console.error(err)
+  core.setFailed(err.message)
+}
 
-repoURL = `https://${token}@github.com/${repo}.git`;
-const simpleGit = require('simple-git')('.');
-simpleGit.clone(repoURL, "repo", ['--depth=1'])
+function clone(repo, token) {
+  const repoURL = `https://${token}@github.com/${repo}.git`;
+  const simpleGit = require('simple-git')('.');
+  simpleGit.clone(repoURL, "repo", ['--depth=1'])
+  console.log("Hello?");
+}
 
-console.log("Hello?");
+async function main() {
+  const personalToken = core.getInput('personal-token');
+  const token = process.env.GITHUB_TOKEN; // TODO: move to input?
+
+  const repo = core.getInput('repo');
+
+  var files = [];
+
+  // Add any single files
+  files = files.concat(core.getInput('file'))
+
+  // Get list of new-line-seperated files
+  if (core.getInput('files')) {
+    files = files.concat(core.getInput('files').split(/\r?\n/))
+  }
+
+  clone(repo, personalToken)
+
+  console.log(repo)
+  console.dir(files)
+}
+
